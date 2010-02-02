81a4c60dc63a6a7c8536bfbf15c52687b5d8a284
Jay <jay@pointcreative.net>
2010-02-02 14:59:40 -0800

    Ready for sorting database work.

--- modules/Contacts/js/view.js
+++ modules/Contacts/js/view.js
@@ -15,8 +15,6 @@ $(document).ready(function(){
 	
 	$('.toggle-group').removeClass('open').addClass('closed').html('Show');
 	
-	
-	
 	$("a.toggle-group").live('click', function() {
 		toggleListItem(this);
 		return false;
@@ -36,93 +34,25 @@ $(document).ready(function(){
 		opacity: 0.5
 	});
 	
-	/**
-	 * Group Management
-	 */
-	// handle add form
-	$('#add-group').submit(function(){
-		status();
-		$.ajax({
-			type: "GET",
-			url: "index.php",
-			data: 'module=Contacts&method=ajax_addGroup&name=' + escape($('#add-group-area input[type=text]').val()),
-			success: function(json){
-				var res = $.evalJSON(json);
-				if(res.success){
-					$('#add-group-area input[type=text]').val('');
-					setTimeout(function() {
-						
-						// prepare new element
-						var new_grp = $('#groupholder-0').clone(false);
-						new_grp.attr('id', 'groupholder-'+res.id);
-						new_grp.find('.group-info').attr('id', 'group_'+res.id+'_details');
-						new_grp.find('.group-info li').remove();
-						new_grp.find('.group-info ul').append( '<li class="empty">There are no contacts in this group.</li>' );
-						new_grp.find('.legend a').attr('id', 'group_'+res.id);
-						new_grp.find('.legend strong').html(res.name);
-						$('#directory-groups').append( new_grp );
-
-				        $.modal.close();
-				    }, 500);
-				} else {
-				
-					$.modal.close();
-					
-				}
-			}
-		});
+	$('.drag').click(function() {
 		return false;
 	});
 	
-	// used to avoid function scope issues
-	var global_id = 0;
-	
-	// delete group link
-	$('#directory-groups a.delete').click(function(){
-		
-		var par_id = $(this).parent().attr('id');
-		par_id = par_id.replace(/group_/, '');
-		par_id = par_id.replace(/_details/, '');
-		global_id = par_id;
-		
-		confirm('Are you sure you want to delete this group?', function(){
-			
-			par_id = global_id;
-			global_id = 0;
-			
-			status();
-			$.ajax({
-				type: "GET",
-				url: "index.php",
-				data: 'module=Contacts&method=ajax_deleteGroup&id=' + par_id,
-				success: function(json){
-					var res = $.evalJSON(json);
-					if(res.success){
-						setTimeout(function() { 
-							$('#groupholder-'+res.id).remove();
-					        $.modal.close();
-					    }, 500);
-						return false;
-					}
-				}
-			});
-		});	
-			
-		return false;
-	});
-	
-	// make names draggable
-	$('#directory-list li').draggable( {
+	$('#directory-list li').addClass('individual').draggable({
 		handle: '.drag',
 		helper: 'clone',
 		appendTo: 'body',
-		revert: true
-	} );
+		revert: 'invalid'
+	});
 	
-	// make groups droppable
+	// make names draggable
+	
+	
+		// make groups droppable
 	$('#directory-groups .group').live('mouseover', function(){
 		
 		$('#directory-groups .group').droppable({
+			accept: '.individual',
 			hoverClass: 'menu-target',
 			drop: function(event, ui){
 	
@@ -156,7 +86,7 @@ $(document).ready(function(){
 									}
 								})
 								
-								$('#group_'+group+'_details ul').append( $('<li class="contact-'+res.id+'">'+res.contact.last_name+', '+res.contact.first_name+'<a class="remove" title="Are you sure you wish to remove this listing?" href="#">Remove</a></li>') );
+								$('#group_'+group+'_details ul').append( $('<li class="contact-'+res.id+' clearfix"><span class="listed">'+res.contact.last_name+', '+res.contact.first_name+'</span><a class="remove" title="Are you sure you wish to remove this listing?" href="#">Remove</a></li>') );
 							}
 						} else {
 							
@@ -175,16 +105,75 @@ $(document).ready(function(){
 		});
 	});
 	
+	// handle add form
+	$('#add-group').submit(function(){
+		status();
+		$.ajax({
+			type: "GET",
+			url: "index.php",
+			data: 'module=Contacts&method=ajax_addGroup&name=' + escape($('#add-group-area input[type=text]').val()),
+			success: function(json){
+				var res = $.evalJSON(json);
+				if(res.success){
+					$('#add-group-area input[type=text]').val('');
+					setTimeout(function() {
+						var new_grp = $('#groupholder-0').clone(false);
+						new_grp.attr('id', 'groupholder-'+res.id);
+						new_grp.find('.group-info').attr('id', 'group_'+res.id+'_details');
+						new_grp.find('.group-info li').remove();
+						new_grp.find('.group-info ul').append( '<li class="empty">There are no contacts in this group.</li>' );
+						new_grp.find('.legend a').attr('id', 'group_'+res.id);
+						new_grp.find('.legend strong').html(res.name);
+						$('#directory-groups').append( new_grp );
+				        $.modal.close();
+				    }, 500);
+				} else {
+					$.modal.close();
+				}
+			}
+		});
+		return false;
+	});
+	
+	// used to avoid function scope issues
+	var global_id = 0;
+	
+	// delete group link
+	$('#directory-groups a.delete').click(function(){
+		var par_id = $(this).parent().attr('id');
+		par_id = par_id.replace(/group_/, '');
+		par_id = par_id.replace(/_details/, '');
+		global_id = par_id;
+		confirm('Are you sure you want to delete this group?', function(){
+			par_id = global_id;
+			global_id = 0;
+			status();
+			$.ajax({
+				type: "GET",
+				url: "index.php",
+				data: 'module=Contacts&method=ajax_deleteGroup&id=' + par_id,
+				success: function(json){
+					var res = $.evalJSON(json);
+					if(res.success){
+						setTimeout(function() { 
+							$('#groupholder-'+res.id).remove();
+					        $.modal.close();
+					    }, 500);
+						return false;
+					}
+				}
+			});
+		});	
+		return false;
+	});
+	
 	// delete member from group link
 	$('#directory-groups li a.remove').live('click', function(){
-		
 		var group_id = $(this).parents('div').attr('id');
 		group_id = group_id.replace(/group_/, '');
 		group_id = group_id.replace(/_details/, '');
-		
 		var contact_id = $(this).parent().attr('class');
 		contact_id = contact_id.replace(/contact-/, '');
-		
 		status();
 		$.ajax({
 			type: "GET",
@@ -205,9 +194,8 @@ $(document).ready(function(){
 		});
 		return false;
 	});
+	
 });
-
-
 
 function enableSorting(trigger){
 	var listItems = trigger.prev('ul').attr('id');
@@ -215,10 +203,10 @@ function enableSorting(trigger){
 		$('#'+listItems).addClass('sorting');
 		$('#'+listItems+' li').stop().animate({backgroundColor: '#383838'});
 		$('#'+listItems+' li .drag').animate({width: "24px", opacity: 'show'}, 500);
-		trigger.html('Disable Sorting');
+		trigger.html('Save');
 		return false;
 	}
-	if($(trigger).html() == 'Disable Sorting'){
+	if($(trigger).html() == 'Save'){
 		$('#'+listItems).removeClass('sorting');
 		$('#'+listItems+' li').stop().animate({backgroundColor: '#282828'});
 		$('#'+listItems+' li .drag').animate({width: "0px", opacity: 'hide'}, 500);
